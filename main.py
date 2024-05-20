4e8ee17741e254de4edfefde75f80074ff4b9115
Nick Spencer <strangebrew202@gmail.com>
2024-05-20 09:26:06 -0600

    Add: learning to use routers in FastAPI and moving models to their own file location

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
+from social_api.routers.post import router as post_router
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-  return {"message": "Hello, world!"}
+app.include_router(post_router)
